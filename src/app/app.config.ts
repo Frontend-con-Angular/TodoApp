2d63b52a0ea49e8b4f079ac1cf6d841a5754e7de
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-12-03 16:39:13 -0500

    feat: setup the guard in the routes and add new route for sign in and sign up

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors} from "@angular/common/http";
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
+import {authenticationInterceptor} from "./iam/services/authentication.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([authenticationInterceptor])),
+    provideAnimationsAsync()
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,27 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { LabsComponent } from './pages/labs/labs.component';
+import { HomeComponent } from './service/pages/home/home.component';
+import {authenticationGuard} from "./iam/services/authentication.guard";
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'labs', component: LabsComponent },
+  {
+    path: '',
+    title: 'home',
+    component: HomeComponent,
+    canActivate: [authenticationGuard]
+  },
+  {
+    path: 'login',
+    title: 'login',
+    loadComponent: () => import('./iam/pages/sign-in/sign-in.component').then(m => m.SignInComponent)
+  },
+  {
+    path: 'register',
+    title: 'register',
+    loadComponent: () => import('./iam/pages/sign-up/sign-up.component').then(m => m.SignUpComponent)
+  },
+  {
+    path : '**',
+    title: 'not-found',
+    loadComponent: () => import('./public/pages/not-found/not-found.component').then(m => m.NotFoundComponent)
+  }
 ];
